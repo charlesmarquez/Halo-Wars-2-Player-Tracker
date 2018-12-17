ad494408e77efd41597bec177a751fb2c24bb5c7
Charles Marquez <charles.marquez@hotmail.com>
2018-12-17 16:40:49 -0500

    Donation button

--- client/src/components/Tracker/App.js
+++ client/src/components/Tracker/App.js
@@ -2,6 +2,7 @@ import React, {Component} from "react";
 import HaloTable from './reactTable'
 import "../../styles/App.css";
 import "react-table/react-table.css"
+import pepe from './pepe.png'
 
 class App extends Component {
     constructor() {
@@ -19,6 +20,15 @@ class App extends Component {
                             <HaloTable
                                 name="DataTable"
                                 data={this.state.data}></HaloTable>
+                        <a href={'https://www.paypal.me/aykonz'} style={{alignContent: 'center'}}>
+                        <img
+                                    className='rounded float-left'
+                                    alt={pepe}
+                                    height={300}
+                                    padding={10}
+                                    src={pepe}
+                                    />
+                        </a>
                         </div>
         );
     }
